5426dc00fa3b179c0b2fc65ae40524053270f6ad
nood32 <yemenmeedo@gmail.com>
2017-04-18 19:17:59 -0400

    Create lock_reply.lua

---
+++ plugins/lock_reply.lua
@@ -0,0 +1,52 @@
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
+do
+
+local function pre_process(msg)
+    
+
+    local hash = 'mate:'..msg.to.id
+    if redis:get(hash) and msg.reply_id and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg) then
+            delete_msg(msg.id, ok_cb, true)
+            return "done"
+        end
+    
+        return msg
+    end
+
+  
+
+
+local function run(msg, matches)
+    chat_id =  msg.to.id
+    
+    if is_momod(msg) and matches[1] == 'قفل' then
+      
+            
+                    local hash = 'mate:'..msg.to.id
+                    redis:set(hash, true)
+                    return ""
+  elseif is_momod(msg) and matches[1] == 'فتح' then
+                    local hash = 'mate:'..msg.to.id
+                    redis:del(hash)
+                    return ""
+end
+
+end
+
+return {
+    patterns = {
+        '^(قفل) الرد$',
+        '^(فتح) الرد$'
+    },
+    run = run,
+    pre_process = pre_process
+}
+end
