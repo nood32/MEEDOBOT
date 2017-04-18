e022dd0ffc0b404e8a3e0265527256d1c6360ad6
nood32 <yemenmeedo@gmail.com>
2017-04-18 19:15:39 -0400

    Create lock_inlin.lua

---
+++ plugins/lock_inlin.lua
@@ -0,0 +1,52 @@
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
+do
+local function pre_process(msg)
+
+    local inline = 'mate:'..msg.to.id
+    if msg.text == '[unsupported]' and redis:get(inline) and not is_momod(msg) then
+            delete_msg(msg.id, ok_cb, true)
+end
+    return msg
+    end
+    
+ local function tshake(msg, matches)
+if is_momod(msg) and matches[1] == "قفل" and matches[2] == "الانلاين" then
+local inline = 'mate:'..msg.to.id
+redis:set(inline, true)
+return "" 
+end
+if is_momod(msg) and matches[1] == "فتح" and matches[2] == "الانلاين" then
+    local inline = 'mate:'..msg.to.id
+    redis:del(inline)
+    return "" 
+  end
+end
+return {
+    patterns ={
+        '^(قفل) (الانلاين)$',
+        '^(فتح) (الانلاين)$',
+        '^[#!/](قفل) (الانلاين)$',
+        '^[#!/](فتح) (الانلاين)$',
+    },
+run = tshake,
+pre_process = pre_process 
+}
+end
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
+
