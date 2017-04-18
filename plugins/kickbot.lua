3f4e92035b26759cd7b97faba23ebfee786393b4
nood32 <yemenmeedo@gmail.com>
2017-04-18 19:02:25 -0400

    Create kickbot.lua

---
+++ plugins/kickbot.lua
@@ -0,0 +1,37 @@
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
+
+do
+
+local function run(msg, matches)
+local bot_id = our_id 
+local receiver = get_receiver(msg)
+    if matches[1] == 'اكيراباي'  and is_admin1(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^(اكيراباي)$",
+    "^[!#/](اكيراباي)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
