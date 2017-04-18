0f6826b68cfb920e022f2efb8d6e468c3efe86dc
nood32 <yemenmeedo@gmail.com>
2017-04-18 18:31:49 -0400

    Create add_dev.lua

---
+++ plugins/add_dev.lua
@@ -0,0 +1,58 @@
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
+local function getindex(t,id) 
+for i,v in pairs(t) do 
+if v == id then 
+return i 
+end 
+end 
+return nil 
+end 
+ 
+function reload_plugins( ) 
+  plugins = {} 
+  load_plugins() 
+end 
+   function h_k_a(msg, matches) 
+    if tonumber (msg.from.id) == 324329857 then 
+       if matches[1]:lower() == "اضف مطور" then 
+          table.insert(_config.sudo_users, tonumber(matches[2])) 
+      print(matches[2] ..'\nتـمـ ☑️ اضـافـه مـطـور فـي الـبـوتـ ❗️') 
+     save_config() 
+     reload_plugins(true) 
+      return matches[2] ..'\nتـمـ ☑️ اضـافـه مـطـور فـي الـبـوتـ ❗️' 
+   elseif matches[1]:lower() == "حذف مطور"  then 
+      local k = tonumber(matches[2]) 
+          table.remove(_config.sudo_users, getindex( _config.sudo_users, k)) 
+      print(matches[2] ..'\nتـمـ ⚠️ حـذفـ الـمـطـور مـن الـبـوتـ ❗️') 
+     save_config() 
+     reload_plugins(true) 
+      return matches[2] ..'\nتـمـ ⚠️ حـذفـ الـمـطـور مـن الـبـوتـ ❗️' 
+      end 
+   end 
+end 
+return { 
+patterns = { 
+"^(اضف مطور) (%d+)$", 
+"^(حذف مطور) (%d+)$",
+"^[#!/](اضف مطور) (%d+)$", 
+"^[#!/](حذف مطور) (%d+)$"
+}, 
+run = run 
+}
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
