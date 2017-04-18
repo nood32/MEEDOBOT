a45aa79b774cd5a7ca1a8525758a42af6c729859
nood32 <yemenmeedo@gmail.com>
2017-04-18 17:49:57 -0400

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,85 @@
+do local _ = {
+  about_text = "",
+  disabled_channels = {
+    ["channel#id1118208265"] = false
+  },
+  enabled_lang = {
+    "arabic_lang",
+    "catalan_lang",
+    "english_lang",
+    "galician_lang",
+    "italian_lang",
+    "persian_lang",
+    "portuguese_lang",
+    "spanish_lang"
+  },
+  enabled_plugins = {
+    "addreplay",
+    "add_dev",
+    "adduser",
+    "admin",
+    "all",
+    "anti_spam",
+    "block",
+    "clean",
+    "broadcast",
+    "banhammer-ar",
+    "broadcast",
+    "delete",
+    "div",
+    "getfile",
+    "getlink",
+    "helo",
+    "helpall",
+    "hema",
+    "info",
+    "kickall",
+    "kickme",
+    "kickbot",
+    "leave_ban",
+    "lock_bot",
+    "lock_ads",
+    "lock_user",
+    "lock_inlin",
+    "lock_tag",
+    "lock_emoji",
+    "lock_cmd",
+    "lock_join",
+    "lock_fwd",
+    "lock_reply",
+    "lock_ads",
+    "lock_media",
+    "lock_edit",
+    "me",
+    "medoskas",
+    "meedo",
+    "msg_checks",
+    "nedme",
+    "newgroup",
+    "note",
+    "plugins",
+    "replay",
+    "robot",
+    "save",
+    "set_time",
+    "wlc",
+    "supergroup",
+    "warnall",
+    "tools",
+    "tophoto",
+    "tosticker",
+    "writer"
+    
+  },
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    324329857,
+   
+  }
+}
+return _
+end
