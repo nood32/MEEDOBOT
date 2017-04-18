2fde64c332c589c73b064d90094dcab4724d4d77
nood32 <yemenmeedo@gmail.com>
2017-04-18 19:20:12 -0400

    Create medoskas.lua

---
+++ plugins/medoskas.lua
@@ -0,0 +1,34 @@
+do 
+
+local function meedo(msg, matches) 
+
+if ( msg.text ) then
+
+  if ( msg.to.type == "user" ) then
+
+     return "للتحدث مع المطور يرجاء مرسلته ع المعرف التالي  #آٰلِِقٓٓـــّنَْـاصْ-مِِيـــツّـــــدُو       @meedo1133"
+  end
+   
+end 
+
+-
+
+end 
+
+return { 
+  patterns = { 
+       "(.*)$"
+  }, 
+  run = meedo, 
+} 
+
+end 
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
