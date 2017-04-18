37200e7d8af3fd4915f4cbd7798435e8b64f8f98
nood32 <yemenmeedo@gmail.com>
2017-04-18 19:50:24 -0400

    Create tosticker.lua

---
+++ plugins/tosticker.lua
@@ -0,0 +1,54 @@
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
+local function tosticker(msg, success, result)
+  local receiver = get_receiver(msg)
+  if success then
+    local file = 'sticker'..msg.from.id..'.webp'
+    print('File downloaded to:', result)
+    os.rename(result, file)
+    print('File moved to:', file)
+    send_document(get_receiver(msg), file, ok_cb, false)
+    redis:del("photo:sticker")
+  else
+    print('Error downloading: '..msg.id)
+    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
+  end
+end
+local function run(msg,matches)
+    local receiver = get_receiver(msg)
+    local group = msg.to.id
+    if msg.reply_id then
+      	if msg.to.type == 'photo' and redis:get("photo:sticker") then
+      		if redis:set("photo:sticker", "waiting") then
+      		end
+      	end
+    
+      if matches[1] == "ملصق" then
+    	redis:get("photo:sticker")  
+        load_photo(msg.reply_id, tosticker, msg)
+    end
+end
+end
+return {
+  patterns = {
+	"^(ملصق)$",
+	"%[(photo)%]",
+  },
+  run = run,
+  } 
+
+--[[
+✄╔═╗╔═╗─────╔═══╗──╔══╗──╔════╗dev:@meedo1133
+✄║║╚╝║║─────╚╗╔╗║──║╔╗║──║╔╗╔╗║dev:@hema1133
+✄║╔╗╔╗╠══╦══╗║║║╠══╣╚╝╚╦═╩╣║║╚╝cha:@meedoyemen
+✄║║║║║║║═╣║═╣║║║║╔╗║╔═╗║╔╗║║║  fbk:@mmalhossainy 
+✄║║║║║║║═╣║═╬╝╚╝║╚╝║╚═╝║╚╝║║║  gml:meedoyemen@gmail.com
+✄╚╝╚╝╚╩══╩══╩═══╩══╩═══╩══╝╚╝  inst:@meedoyemen
+]]--
